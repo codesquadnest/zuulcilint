89087decd3fd851c8ade83ef3121836415da1f57
root <root@DESKTOP-GM7HCFV>
2024-02-17 23:22:21 +0000

    check-secrets: Check if the secret from the job exist
    Issue: 23

--- zuulcilint/checker.py
+++ zuulcilint/checker.py
@@ -23,9 +23,7 @@ def check_job_playbook_paths(
     for key in ["pre-run", "run", "post-run"]:
         paths = job.get(key)
         if isinstance(paths, list):
-            invalid_paths.extend(
-                path for path in paths if not pathlib.Path(path).is_file()
-            )
+            invalid_paths.extend(path for path in paths if not pathlib.Path(path).is_file())
         elif paths and not pathlib.Path(paths).is_file():
             invalid_paths.append(paths)
 
@@ -58,3 +56,63 @@ def check_duplicated_jobs(
         unique_items.update(sublist_set)
 
     return duplicated_items
+
+
+def get_all_project_secrets(
+    secrets: list[dict],
+) -> list[dict[str, str]]:
+    """
+    Get all encrypted secrets.
+
+    Args:
+    ----
+        secrets: A list of secrets.
+    Returns:
+    -------
+        A list of encrypted secrets.
+    """
+
+    # TODO: This function need improvement
+
+    secrets_list = set()
+    for secret in secrets:
+        secrets_list.add(secret["secret"]["name"])
+        try:
+            secret_list = secrets["secret"]["data"]
+        except KeyError:
+            continue
+        for secret in secret_list:
+            if "!encrypted/pkcs1-oaep" not in secret["secret"]["name"][secret]:
+                print("Error: The secret don't use the Zuul encryption scheme")
+                sys.exit(1)
+
+    return secrets_list
+
+
+def check_inexistent_secrets(
+    secrets: list[dict],
+    jobs: list[dict | None],
+) -> list[dict[str, str]]:
+    """
+    Check that secret used by the job exist.
+
+    Args:
+    ----
+        secrets: A list of secrets.
+        jobs: A list of jobs.
+    Returns:
+    -------
+        A list of inexistent secrets.
+    """
+    inexistent_secrets = set()
+    job_secrets_list = set()
+    secret = {}
+
+    for job in jobs:
+        try:
+            if isinstance(job["job"]["secrets"], str):
+                secret["name"] = job["job"]["secrets"]["secret"]
+                job_secrets_list.add(secret)
+        except KeyError:
+            continue
+    return inexistent_secrets
